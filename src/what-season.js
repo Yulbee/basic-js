a9c235afdada7be1d14c34f81a378a2248a6575a
Yulbee <koko_joko@rambler.ru>
2023-04-15 00:22:01 +0300

    fix: check what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -12,39 +12,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  let season = date.getMonth();
   if (date === undefined) {
     return 'Unable to determine the time of year!';
   }
-else {
-  switch (season) {
-    case 11:
-    case 0:
-    case 1:
-      return 'winter';
-  
-    case 2:
-    case 3:
-    case 4:
-     return 'spring';
-  
-    case 5:
-    case 6:
-    case 7:
-     return 'summer';
-  
-    case 8:
-    case 9:
-    case 10:
-     return 'autumn';
-    default: 
-    throw new Error ('Invalid date!');
-      
-  }
-}
-   
-  }
+  if (!(date instanceof Date) || date.hasOwnProperty("toString")) {
+     throw new Error('Invalid date!');
+    }
 
+    let season = date.getMonth();
+    switch (season) {
+      case 11:
+      case 0:
+      case 1:
+        return 'winter';
+    
+      case 2:
+      case 3:
+      case 4:
+       return 'spring';
+    
+      case 5:
+      case 6:
+      case 7:
+       return 'summer';
+    
+      case 8:
+      case 9:
+      case 10:
+       return 'autumn';
+       default:
+       throw new Error('Invalid date!');
+    }
+  }
 
 module.exports = {
   getSeason
